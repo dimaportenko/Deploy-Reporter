aa27b524bfb2f965750071ee354721a25b0e2d73
Dmytro Portenko <troublesohard@gmail.com>
2017-05-31 21:32:46 +0300

    add setup variables
    -----END RSA PRIVATE KEY-----

--- git_status_modify.sh
+++ git_status_modify.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-if git diff-index --name-status --exit-code HEAD
-then
+project_name=sample
+reciever_email=sample@gmail.com
+email_subject="Alert. Git contains modified files"
+
+if git diff-index --name-status --exit-code HEAD; then
     echo Git working copy is clean...;
 else
-    echo ;
-    echo ERROR: Git working copy is dirty!;
-    echo Commit your changes and try again.;
-
-    mail -s "Alert git copy isn't clear" address@gmail.com <<< "$(git diff-index --name-status --exit-code HEAD)"
-
+    git_output="$(git diff-index --name-status --exit-code HEAD)"
+    message=$(printf "project name - $project_name \n$(date +%d-%m-%Y) \n$git_output")
+    mail -s "$email_subject" $reciever_email <<< "$message"
+#    echo "$message"
 fi;
